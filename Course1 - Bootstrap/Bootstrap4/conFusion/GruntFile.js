825836f7ebf17a4f572e81b1ea04520ca7d104ee
David Liang <davidxliang@gmail.com>
2019-06-22 12:41:14 -0500

    submitting and pushing the course 1 codes before starting course 2

---
+++ Course1 - Bootstrap/Bootstrap4/conFusion/GruntFile.js
@@ -0,0 +1,87 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    require('time-grunt')(grunt);
+
+    require('jit-grunt')(grunt);
+        
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'css/styles.css' :'css/styles.scss'
+                }
+            }
+        },
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: "./"
+                    }
+                }
+            }
+        },
+        copy: {
+            html: {
+                files: [
+                {
+                    //for html
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]                
+            },
+            fonts: {
+                files: [
+                {
+                    //for font-awesome
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            }
+        },
+
+        clean: {
+            build: {
+                src: [ 'dist/']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: './',                   // Src matches are relative to this path
+                    src: ['img/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'dist/'                  // Destination path prefix
+                }]
+            }
+        }
+    });
+
+    grunt.registerTask('css', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registertask('build', [
+        'clean',
+        'copy',
+        'imagemin'
+    ]);
+};
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Full-Stack-Coursera
 Trying to improve as a full stack developer and I figured I should do something that is closest to the basics before going to a full React course
+
+This is a coursera specialization pathway:
+Full Stack Web and Multiplatform Mobile App Development Specialization : https://www.coursera.org/specializations/full-stack-mobile-app-development
+
